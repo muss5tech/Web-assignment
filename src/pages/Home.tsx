b5bc733bf03d990f8c553755fdeffbca76071509
muss5tech <mus@s5tech.co>
2025-11-27 21:02:41 +0700

    feat: setup react-router and page structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import HeroSection from '../components/sections/HeroSection';
+import TechStackSection from '../components/sections/TechStackSection';
+import ExperienceSection from '../components/sections/ExperienceSection';
+import ProjectsSection from '../components/sections/ProjectsSection';
+import ContactSection from '../components/sections/ContactSection';
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+      <TechStackSection />
+      <ExperienceSection />
+      <ProjectsSection />
+      <ContactSection />
+    </>
+  );
+}
+
+export default Home;
